f2b475e3911114feb0ba23ceeba7974a7cf59840
divrhino <fancydresspenguin+divrhino@gmail.com>
2021-03-24 20:51:24 +0800

    Handle error returned from OutputFileAndClose

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/johnfercher/maroto/pkg/consts"
 	"github.com/johnfercher/maroto/pkg/pdf"
 )
@@ -9,5 +12,9 @@ func main() {
 	m := pdf.NewMaroto(consts.Portrait, consts.A4)
 	m.SetPageMargins(20, 10, 20)
 
-	m.OutputFileAndClose("pdfs/div_rhino_fruit.pdf")
+	err := m.OutputFileAndClose("pdfs/div_rhino_fruit.pdf")
+	if err != nil {
+		fmt.Println("⚠️  Could not save PDF:", err)
+		os.Exit(1)
+	}
 }
